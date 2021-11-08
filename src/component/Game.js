48fe39e0439a14dde17e63ff450721f32bcdc36a
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-11-08 16:24:02 +0900

    add:function calculateWinner|| fix:compile error

--- src/component/Game.js
+++ src/component/Game.js
@@ -3,6 +3,25 @@ import Board from "./Board"
 
 
 const Game = () => {
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
     const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
     const [xIsNext, setXIsNext] = useState(true);
     const handleClick = (i) => {
