70afefa726cb0d5a494e37fcd55ee1c0ffe8b8ca
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-11-08 17:10:57 +0900

    delete: unused text

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
     <div className="App">
       <Game></Game>
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
       </header>
     </div>
